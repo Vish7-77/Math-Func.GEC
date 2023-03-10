c1b5dd55e10287f16490c5ca071f4a075431f36b
vishal <Vishalgehlot334@gmail.com>
2023-03-10 16:47:33 +0530

    ok

---
+++ script.js
@@ -0,0 +1,135 @@
+
+// //javascript is an Async Language : Single Threaded
+
+
+
+// let x=20
+// let y=10
+
+// function Hello(a,b){
+
+
+// return a+b
+
+// }
+
+// console.log('hello')
+
+// let result = Hello(x,y)
+
+
+// console.log(result)
+
+
+
+
+
+// //GEC   2 phases Memory Allocation  & Execuation Context
+
+
+
+
+// // ->x ,y , Hello ,result
+// //->Execute  console Hello(x,y),result
+// //hello execution ->
+
+
+// //stacks --- EC   -- GEC ,Hello()
+
+
+// console.log('hee')
+
+// let a=10
+
+// console.log(a)
+
+
+
+//memory allocation :  Firstly for all the variables the Values are Undefined
+
+
+
+
+//Queues :- async :     if u have 
+
+
+// let a =10
+// let b=20
+
+
+
+
+
+// function Hell(){
+//     for(let i=0; i<1000; ++i){
+//         console.log(i)
+//     }
+    
+    
+// }
+
+
+
+// setTimeout(Hell, 0);
+
+
+
+// // Hell()
+// console.log('hello')
+
+
+//Math
+
+
+// for (let i=0; i<2; ++i){
+// //   console.log(1+  Number((Math.random()*15).toFixed(0)))
+// }
+
+
+// let x=-10.33 
+
+
+//round off
+// console.log(Math.round(x))
+
+
+
+//upper
+// console.log(Math.ceil(x))
+
+
+//lower
+// console.log(Math.floor(x))
+
+//positive value
+// console.log(Math.abs(a-b))
+
+
+
+// function to how we use Math.abs
+// function Add (a,b){
+
+//     console.log(Math.abs(a-b))
+// }
+
+
+// Add(12,900)
+
+// finding the Square/power of a number 
+// console.log(Math.pow(2,3))
+
+
+
+// this to find out Square root of A number
+// console.log(Math.sqrt(144))
+
+//Value of PI
+// console.log(Math.PI)
+
+// console.log(22/7)
+
+
+
+
+
+
